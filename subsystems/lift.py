2c34ce867b0813d22501d723f764c3aec8569752
Devin Mozee <alonzo.mozee@gmail.com>
2016-03-09 19:51:09 -0500

    Reworked Lift.py (subject to change)

--- subsystems/lift.py
+++ subsystems/lift.py
@@ -14,9 +14,9 @@ class LiftMech(Subsystem):
 
         self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.Position)
 
-        self.armMotor.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.AnalogPot)
+        self.armMotor.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
 
-        self.armMotor.setPID(0.5,0.0,0.0)
+        self.armMotor.setPID(1.0,0.0,0.0)
 
         self.armMotor.setVoltageRampRate(24.0/0.9)
         self.sd = wpilib.SmartDashboard
@@ -28,26 +28,49 @@ class LiftMech(Subsystem):
         '''
             This in theory will lift the arm to the top of the robot
         '''
-        self.armMotor.set(400)
+        self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.Position)
+        self.armMotor.setPosition(-2999)
 
     def moveDown(self):
-       self.armMotor.set(150)
+       self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.Position)
+       self.armMotor.setPosition(4999)
 
     def stopmovement(self):
         self.armMotor.set(0)
 
-    def moveref(self, joy_lift):
-        if joy_lift.getRawAxis(1) == 1:
+
+    def limitPOS(self):
+        if self.armMotor.getEncPosition() <= -3000:
             self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.PercentVbus)
-            self.armMotor.set(joy_lift.getRawAxis(1)*0.4)
-        elif joy_lift.getRawAxis(1) == -1:
+            self.armMotor.set(0)
+            return True
+        elif self.armMotor.getEncPosition() >= 5000:
+            self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.PercentVbus)            
+            self.armMotor.set(0)
+            return True
+        else:
+            return False
+
+    def limitme(self):
+        if self.limitPOS() == True and self.armMotor.getEncPosition() <= -3000:
+            self.Moveup()
+        elif self.limitPOS() == True and self.armMotor.getEncPosition() >= 5000:
+            self.moveDown()
+
+    def moveref(self, joy_lift):
+
+        if joy_lift.getRawAxis(1) == 1 and self.limitPOS() == False:
+            self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.PercentVbus)
+            self.armMotor.set(joy_lift.getRawAxis(1)*0.4) 
+        elif joy_lift.getRawAxis(1) == -1 and self.limitPOS() == False:
             self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.PercentVbus)
             self.armMotor.set(joy_lift.getRawAxis(1)*0.4)
         else:
             self.armMotor.changeControlMode(wpilib.CANTalon.ControlMode.PercentVbus)
             self.armMotor.set(0)
+            self.limitme()
 
 
     def log(self):
         self.sd.putDouble("Arm Distance", self.armMotor.getEncPosition())
-        self.sd.putDouble("Arm Analog", self.armMotor.getAnalogInVelocity())
+        self.sd.putDouble("Arm Analog", self.armMotor.get())
